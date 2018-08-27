d62bcfd9f72b5481c1b2648cfd3f0f504f47b46e
PolarisJunior <brownasianpj@yahoo.com>
2018-08-26 17:46:33 -0700

    load texture image

---
+++ sdl-image/sdl-image-constants.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+
+(define IMG_INIT_JPG #x00000001)
+(define IMG_INIT_PNG #x00000002)
+(define IMG_INIT_TIF #x00000004)
+(define IMG_INIT_WEBP #x00000008)
+
--- sdl-image/sdl-image.rkt
+++ sdl-image/sdl-image.rkt
@@ -1,12 +1,18 @@
 #lang racket
 (require ffi/unsafe
          ffi/unsafe/define)
+(require "sdl-image-constants.rkt")
 
 (provide
  (all-defined-out))
 
+(require "../sdl/structs.rkt")
 
 
 (define-ffi-definer define-sdl-image (ffi-lib "SDL2_image" #f))
 
 (define-sdl-image IMG_Init (_fun _int -> _int))
+
+(define-sdl-image IMG_Quit (_fun -> _void))
+
+(define-sdl-image IMG_LoadTexture (_fun _SDL_Renderer _string -> _SDL_Texture))
